713d6dab14117c36b26cc5f3e0ee21e751745b0e
Saikal Kuttubekova <saikal.kuttubekova.on@gmail.com>
2022-03-10 21:11:32 +0600

    JSe6

--- js/script.js
+++ js/script.js
@@ -1,56 +1,110 @@
 /* Book Constructor */
-function Book(title, author, isbn) {
- this.title = title;
- this.author = author;
- this.isbn = isbn;
-};
+// function Book(title, author, isbn) {
+//  this.title = title;
+//  this.author = author;
+//  this.isbn = isbn;
+// };
 
 /* UI Constructor  */
-function UI() {}
+// function UI() {}
 
-/* UI Prototype Methods */
-UI.prototype.addBookToList = function (book) {
- const list = document.getElementById("book-list");
- const row = document.createElement("tr");
+// /* UI Prototype Methods */
+// UI.prototype.addBookToList = function (book) {
+//  const list = document.getElementById("book-list");
+//  const row = document.createElement("tr");
 
- row.innerHTML = `
- <td>${book.title}<td/>
- <td>${book.author}<td/>
- <td>${book.isbn}<td/>
- <td><a href="#" class="delete">X</a><td/>
- `;
+//  row.innerHTML = `
+//  <td>${book.title}<td/>
+//  <td>${book.author}<td/>
+//  <td>${book.isbn}<td/>
+//  <td><a href="#" class="delete">X</a><td/>
+//  `;
 
-  list.appendChild(row);
-};
+//   list.appendChild(row);
+// };
 
 //show alert
-UI.prototype.showAlert = function (msg, className) {
-    const div = document.createElement('div');
-    div.className = `alert ${className}`;
-    div.appendChild(document.createTextNode(msg));
+// UI.prototype.showAlert = function (msg, className) {
+//     const div = document.createElement('div');
+//     div.className = `alert ${className}`;
+//     div.appendChild(document.createTextNode(msg));
 
-    const container = document.querySelector(".container");
-    const form = document.querySelector("#book-form");
+//     const container = document.querySelector(".container");
+//     const form = document.querySelector("#book-form");
 
-    container.insertBefore(div, form);
+//     container.insertBefore(div, form);
 
-    setTimeout(function(){
-        document.querySelector(".alert").remove();
-    }, 3000);
-};
+//     setTimeout(function(){
+//         document.querySelector(".alert").remove();
+//     }, 3000);
+// };
  
 //clear fields
-UI.prototype.clearFields = function () {
+// UI.prototype.clearFields = function () {
+//     document.getElementById("title").value = "";
+//     document.getElementById("author").value = "";
+//     document.getElementById("isbn").value = "";
+// };
+// deletebook
+// UI.prototype.deleteBook = function (target) {
+//  if (target.className === "delete") {
+//      target.parentElement.parentElement.remove();
+//  }
+// };
+
+
+
+ class Book{
+     constructor(title, author, isbn){
+         this.title = title;
+         this.author = author;
+         this.isbn = isbn;
+     }
+ }
+
+ class UI {
+
+     addBookToList(book) {
+        const list = document.getElementById("book-list");
+        const row = document.createElement("tr");
+       
+        row.innerHTML = `
+        <td>${book.title}<td/>
+        <td>${book.author}<td/>
+        <td>${book.isbn}<td/>
+        <td><a href="#" class="delete">X</a><td/>
+        `;
+       
+         list.appendChild(row);
+        }
+
+     showAlert(msg, className) {
+        const div = document.createElement("div");
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(msg));
+    
+        const container = document.querySelector(".container");
+        const form = document.querySelector("#book-form");
+    
+        container.insertBefore(div, form);
+    
+        setTimeout(function() {
+            document.querySelector(".alert").remove();
+        }, 3000);
+     }
+ 
+    deleteBook(target) {
+        if (target.className === "delete") {
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    clearFields() {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
     document.getElementById("isbn").value = "";
-};
-// deletebook
-UI.prototype.deleteBook = function (target) {
- if (target.className === "delete") {
-     target.parentElement.parentElement.remove();
- }
-};
+    };
+}
 
 //event listeners
 document.getElementById("book-form").addEventListener("submit", (e) => {
@@ -76,12 +130,14 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
 
     e.preventDefault();
 });
+
+
  // event listener for DBook
  document.getElementById("book-list").addEventListener("click", (e) => {
-     const ui = new UI();
-     ui.deleteBook(e.target);
-     ui.showAlert("Book Deleted", "success");
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert("Book Deleted", "success");
 
 
-     e.preventDefault();
- } );
+    e.preventDefault();
+} );
